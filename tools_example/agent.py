00e1c00917fd06ad9b0d87cee12bd5dcf30b87df
Aman Shrestha <amanshrestha@google.com>
2026-05-23 22:19:23 +0000

    added tools_example

---
+++ tools_example/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents.llm_agent import Agent
+
+
+from .sub_agents import gce_agent, gcs_agent
+
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description='A root agent that delegates questiosn to different agents',
+    instruction='When you get a question, send it either to the gce_agent or gcs_agent. Never answer yourself unless the question is about neither gce or gcs',
+    sub_agents=[
+        gce_agent,
+        gcs_agent,
+    ]
+)
---
+++ tools_example/sub_agents/gcs_agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents.llm_agent import Agent
+
+gcs_agent = Agent(
+    model='gemini-2.5-flash',
+    name='gcs_agent',
+    description='A helpful assistant for user questions on google cloud storage.',
+    instruction='Answer user questions to the best of your knowledge about google cloud storage',
+)
